c291fc02ab326f631b360030385e30449ca973ba
BoltR6 <johnwallacewhite@gmail.com>
2024-02-01 11:05:25 -0600

    Fixed command names

--- server.py
+++ server.py
@@ -522,7 +522,7 @@ async def on_message(message):
         name_map_file.close()
 
         match cmd.pop(0):
-            case "decon":
+            case "econ":
                 match cmd.pop(0):
                     case "list":
                         msg = ""
@@ -543,7 +543,7 @@ async def on_message(message):
                         all_time_RAP = round(sum(cleaned_data) / max(1, sold_len))
 
                         msg = f'# Buy:\n\tMinimum Buyer: **{_data["data"][0]}** R6 credits\n\tMaximum Buyer: **{_data["data"][1]}** R6 credits\n\tVolume Buyers: **{_data["data"][2]}**\n'
-                        msg += f'# Sell:\n\tMinimum Seller: **{_data["data"][3]}** R6 credits\n\tMaximum Seller: **{_data["data"][4]}** R6 credits\n\tVolume Buyers: **{_data["data"][5]}**\n\tLast Sold: **{_data["sold"][-1][0]}**\n\n'
+                        msg += f'# Sell:\n\tMinimum Seller: **{_data["data"][3]}** R6 credits\n\tMaximum Seller: **{_data["data"][4]}** R6 credits\n\tVolume Sellers: **{_data["data"][5]}**\n\tLast Sold: **{_data["sold"][-1][0]}**\n\n'
                         msg += f'### Quick Analysis:\n\tHighest Buyer vs. Lowest Seller: **{_data["data"][3] - _data["data"][1]}** R6 credits\n\tLast Sale vs. Lowest Seller: **{_data["data"][3] - _data["sold"][-1][0]} ({round(100 -(_data["sold"][-1][0] / _data["data"][3]) * 100, 2)}%)** R6 credits\n'
                         msg += f'### RAP:\n\t10 - **{ten_RAP}**\n\t100 - **{hundred_RAP}**\n\tAll Time - **{all_time_RAP}**\n\n\t*(Total Data: {sold_len})*\n### Tags:\n\n{_data["tags"]}\n### Item ID:\n\t{item_id}'
                         embed=discord.Embed(title=f'{_data["name"]} ({_data["type"]})', url=f'https://www.ubisoft.com/en-us/game/rainbow-six/siege/marketplace?route=buy%252Fitem-details&itemId={item_id}', description=f'{msg}', color=0xFF5733)
@@ -560,7 +560,7 @@ async def on_message(message):
                         all_time_RAP = round(sum(cleaned_data) / max(1, sold_len))
 
                         msg = f'# Buy:\n\tMinimum Buyer: **{_data["data"][0]}** R6 credits\n\tMaximum Buyer: **{_data["data"][1]}** R6 credits\n\tVolume Buyers: **{_data["data"][2]}**\n'
-                        msg += f'# Sell:\n\tMinimum Seller: **{_data["data"][3]}** R6 credits\n\tMaximum Seller: **{_data["data"][4]}** R6 credits\n\tVolume Buyers: **{_data["data"][5]}**\n\tLast Sold: **{_data["sold"][-1][0]}**\n\n'
+                        msg += f'# Sell:\n\tMinimum Seller: **{_data["data"][3]}** R6 credits\n\tMaximum Seller: **{_data["data"][4]}** R6 credits\n\tVolume Sellers: **{_data["data"][5]}**\n\tLast Sold: **{_data["sold"][-1][0]}**\n\n'
                         msg += f'### Quick Analysis:\n\tHighest Buyer vs. Lowest Seller: **{_data["data"][3] - _data["data"][1]}** R6 credits\n\tLast Sale vs. Lowest Seller: **{_data["data"][3] - _data["sold"][-1][0]} ({round(100 -(_data["sold"][-1][0] / _data["data"][3]) * 100, 2)}%)** R6 credits\n'
                         msg += f'### RAP:\n\t10 - **{ten_RAP}**\n\t100 - **{hundred_RAP}**\n\tAll Time - **{all_time_RAP}**\n\n\t*(Total Data: {sold_len})*\n### Tags:\n\n{_data["tags"]}\n### Item ID:\n\t{item_id}'
                         embed=discord.Embed(title=f'{_data["name"]} ({_data["type"]})', url=f'https://www.ubisoft.com/en-us/game/rainbow-six/siege/marketplace?route=buy%252Fitem-details&itemId={item_id}', description=f'{msg}', color=0xFF5733)
@@ -618,7 +618,7 @@ async def on_message(message):
                         e.set_image(url=f'attachment://{item_id}.png')
                         await message.channel.send(file = file, embed=e)
                     case _:
-                        msg = "The following commands are available:\n\t- econ name <item name>\n\t- econ id <item id>\n\t- econ graph <# entries (1, 2, ... | all)> <unit (days | hours | minutes)>"
+                        msg = "The following commands are available:\n\n\t- econ name <item name>\n\n\t- econ id <item id>\n\n\t- econ graph <# entries (1, 2, ... | all)> <unit (days | hours | minutes)>"
                         embed=discord.Embed(title=f'Help', description=f'# Ask @hiibolt on GH/DC for help!\n\n# Skins:\n{msg}', color=0xFF5733)
                         embed.set_thumbnail(url="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwallpapercave.com%2Fwp%2Fwp7511401.png&f=1&nofb=1&ipt=774c2f1e44a99d33a82af5645f290c48fb316c0f43af86f11b4f167eb70d8a0a&ipo=images")
                         await message.channel.send(embed=embed)
